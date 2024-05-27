51d451b3d45fa9bb506b528209a46f8cbf634eee
Lucas <lucasjfelippo@gmail.com>
2024-05-27 04:52:16 -0300

    Base setup done

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import Home from '../views/Home.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: Home }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes
+})
+
+export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
 </script>
 
 <template>
-    <RouterView />
+    <h1>Home</h1>
 </template>
 
 <style scoped>
