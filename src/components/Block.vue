8b1208f6735bc90bafb07cd7f9ca069d7a640c67
Lucas <lucasjfelippo@gmail.com>
2024-05-31 13:15:46 -0300

    Update

---
+++ src/components/Block.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+interface Props {
+  title?: string
+  reverse?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: 'Hello World!',
+  reverse: false
+})
+</script>
+
+<template>
+    <div class="block">
+        <div :class="['content', props.reverse ? 'reverse-row' : 'row']">
+            <div class="text">
+                <h1>{{ props.title }}</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ipsa autem sit labore enim.</p>
+            </div>
+            <div class="img">
+                <img src="../assets/images/marketplace_placeholder.webp" alt="placeholder">
+            </div>
+        </div>
+        <div class="divider"></div>
+    </div>
+</template>
+
+<style scoped>
+
+.block {
+    position: relative;
+    min-height: auto;
+    padding: 4.5rem 0;
+
+    display: flex;
+    justify-content: center;
+}
+
+.content {
+    max-width: calc(80% - 4rem);
+    margin: auto;
+
+    display: flex;
+    flex-direction: column;
+
+    text-align: center;
+}
+
+.content .text, .content .img {
+    flex-basis: 50%;
+}
+.content h1 {
+    font-size: 2rem;
+    margin: 0;
+}
+.content p {
+    font-size: 1.1rem;
+    margin: 1rem 0 0;
+}
+.content img {
+    width: 100%;
+    height: 200px;
+}
+
+@media screen and (min-width: 960px) {
+    .content {
+        flex-direction: row;
+        text-align: left;
+    }
+    .row {
+        flex-direction: row;
+    }
+    .reverse-row {
+        flex-direction: row-reverse
+    }
+    .reverse-row .text {
+        padding-left: 1rem;
+    }
+    .content h1 {
+        font-size: 3rem;
+    }
+    .content p {
+        font-size: 1.5rem;
+    }
+}
+
+/* .block .img-div {
+    --image-height: 200px;
+
+    position: relative;
+    top: 50%;
+    left: 20%;
+    transform: translate(0, -50%);
+
+    width: 200px;
+    height: var(--image-height);
+}
+.block .img-div img {
+    width: 100%;
+    height: 100%;
+}
+.block .img-div div {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+
+
+.block .text {
+    position: relative;
+    left: calc(20% + var(--image-height) + 100px);
+
+    width: 50%;
+    height: 50%;
+    background-color: white;
+} */
+
+
+.block .divider {
+    position: absolute;
+    width: 30%;
+    height: 0.3em;
+    left: 50%;
+    transform: translate(-50%);
+
+    background-color: rgba(var(--text-color-base), 1);
+
+    border-radius: 100%;
+
+    bottom: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,104 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Block from '../components/Block.vue';
+
+import { setWorld } from '../assets/scripts/cube_world/main'
+
+// const world = ref();
+
+// onMounted(() => {
+//     setWorld(world.value)
+// });
+
 </script>
 
 <template>
-    <h1>Home</h1>
+    <div id="presentation">
+        <div id="background">
+            <img aria-hidden="true" src="../assets/images/presentation_background.webp">
+            <div></div>
+        </div>
+
+        <div id="world" ref="world"></div>
+
+        <div id="text">
+            <h1>
+                <span class="text-functions-color">Print</span>
+                <span class="text-brackets-color">(</span>
+                <span class="text-string-color">"Hello World!"</span>
+                <span class="text-brackets-color">)</span>
+            </h1>
+            <h1>
+                <span class="text-functions-color">Name </span>
+                <span class="text-symbol-color">= </span>
+                <span class="text-string-color">“Lucas J. Felippo”</span>
+            </h1>
+            <h1>
+                <span clas="text-color">Junior Full-Stack Engineer</span>
+            </h1>
+            <p>
+                /* Bringing together the blocks that made your businnes soar since 2020. From Web Development to Artificial Intelligence, it’s the adventure to discover new thing to bring solutions to everyday problems that drives me.
+                */
+            </p>
+        </div>
+    </div>
+    <Block title="Test"/>
+    <Block title="Test2" :reverse="true"/>
+    <Block />
+    <Block />
 </template>
 
 <style scoped>
+#presentation {
+    position: relative;
+
+    width: 100%;
+    height: 95vh;
+}
+#presentation #background {
+    position: absolute;
+    width: 100%;
+    height: 99%;
+}
+#presentation #background img {
+    width: 100%;
+    height: 100%;
+
+    object-fit: cover;
+    /* filter: blur(4px) */
+}
+#presentation #background div {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    background-color: rgb(var(--presentation-bg-color));
+    /* background-image:linear-gradient(to top, rgba(var(--background-color), 1) 0%, rgba(var(--background-color), 0) 50%, rgba(var(--background-color), 1) 100%); */
+}
+#presentation #text {
+    position: absolute;
+    top: 50%;
+    left: 10%;
+    transform: translate(0, -50%);
+
+    width: 50%;
+}
+#presentation #text h1 {
+    font-size: 2.8rem;
+    
+    margin: 0;
+}
+#presentation #text p {
+    font-size: 1.5rem;
+
+    margin: 0;
+}
+
+#world {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
 </style>
